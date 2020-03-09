ce87c350ca7c1161ccce57aac86981c6865377f8
pkdism <pawan.dwivedi94@gmail.com>
2020-03-09 16:22:07 +0530

    pyplot

--- matplotlib.py
+++ matplotlib.py
@@ -80,6 +80,192 @@ my_plotter(ax2, data3, data4, {'marker': 'o'})
 
 
 
+# PYPLOT
+
+plt.plot([1,2,3,4]) # in case of single array, values are y values and x are taken by default - starting from 0
+plt.ylabel('some numbers')
+plt.show()
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16]) # first array has x values and second has y values
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro') # ro stands for red o's or circles
+plt.axis([0, 6, 0, 20]) # sets xlim and ylim
+plt.show()
+
+
+# evenly sampled time at 200ms intervals
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^') # r-- red dashes, bs - blue squares, g^ green triangles
+plt.show()
+
+
+data = {'a': np.arange(50),
+        'c': np.random.randint(0, 50, 50),
+        'd': np.random.randn(50)}
+data['b'] = data['a'] + 10 * np.random.randn(50)
+data['d'] = np.abs(data['d']) * 100
+
+plt.scatter('a', 'b', c='c', s='d', data=data) # c stands for color and s stands for size
+plt.xlabel('entry a')
+plt.ylabel('entry b')
+plt.show()
+
+
+# Categorical variables
+names = ['group_a', 'group_b', 'group_c']
+values = [1, 10, 100]
+
+plt.figure(figsize=(9, 3))
+plt.subplot(131) # number of rows = 1, number of columns = 3, plot number = 1
+plt.bar(names, values)
+plt.subplot(132) # number of rows = 1, number of columns = 3, plot number = 2
+plt.scatter(names, values)
+plt.subplot(133) # number of rows = 1, number of columns = 3, plot number = 3
+plt.plot(names, values)
+plt.suptitle('Categorical Plotting')
+plt.show()
+plt.show()
+
+
+x = list(range(20))
+y = [i ** 2 for i in x]
+plt.plot(x, y, linewidth=2.0)
+
+
+line, = plt.plot(x, y, '-')
+line.set_antialiased(False) # turn off antialiasing
+
+# Creating multiple figures and axes
+def f(t):
+    return np.exp(-t) * np.cos(2*np.pi*t)
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+plt.figure() # optional
+plt.subplot(211) # number of rows = 2, number of columns = 1, plot number = 1
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212) # number of rows = 1, number of columns = 3, plot number = 2
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
+
+
+plt.figure(1)                # the first figure
+plt.subplot(211)             # the first subplot in the first figure
+plt.plot([1, 2, 3])
+plt.subplot(212)             # the second subplot in the first figure
+plt.plot([4, 5, 6])
+
+plt.figure(2)                # a second figure
+plt.plot([4, 5, 6])          # creates a subplot(111) by default
+
+plt.figure(1)                # figure 1 current; subplot(212) still current
+plt.subplot(211)             # make subplot(211) in figure1 current
+plt.title('Easy as 1, 2, 3') # subplot 211 title
+
+# cla() # clears current axes
+# clf() # clears currect figure
+
+
+# Working with texts
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
+
+t = plt.xlabel('my data', fontsize=14, color='red')
+plt.title(r'$\sigma_i=15$') # using LaTeX notations
+
+
+# Annotations
+ax = plt.subplot(111)
+t = np.arange(0.0, 5.0, 0.01)
+s = np.cos(2*np.pi*t)
+line, = plt.plot(t, s, lw=2)
+plt.annotate('local max', xy=(2, 1), xytext=(3, 1.5), 
+             arrowprops=dict(facecolor='black', shrink=0.05),
+             ) # xy arguement is part of the plot to point to and xytext is place of text
+
+plt.ylim(-2, 2)
+plt.show()
+
+
+# Logarithmic and non-linear axes
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# make up some data in the open interval (0, 1)
+y = np.random.normal(loc=0.5, scale=0.4, size=1000)
+y = y[(y > 0) & (y < 1)]
+y.sort()
+x = np.arange(len(y))
+
+# plot with various axes scales
+plt.figure()
+
+# linear
+plt.subplot(221)
+plt.plot(x, y)
+plt.yscale('linear')
+plt.title('linear')
+plt.grid(True)
+
+
+# log
+plt.subplot(222)
+plt.plot(x, y)
+plt.yscale('log')
+plt.title('log')
+plt.grid(True)
+
+
+# symmetric log
+plt.subplot(223)
+plt.plot(x, y - y.mean())
+plt.yscale('symlog', linthreshy=0.01)
+plt.title('symlog')
+plt.grid(True)
+
+# logit
+plt.subplot(224)
+plt.plot(x, y)
+plt.yscale('logit')
+plt.title('logit')
+plt.grid(True)
+# Adjust the subplot layout, because the logit one may take more space
+# than usual, due to y-tick labels like "1 - 10^{-3}"
+plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
+                    wspace=0.35)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
