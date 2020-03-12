97f8cf117fc621b504febb433eaf83a217b75676
pkdism <pawan.dwivedi94@gmail.com>
2020-03-12 19:24:53 +0530

    lifecyle of a plot

--- matplotlib.py
+++ matplotlib.py
@@ -300,37 +300,154 @@ plt.colorbar(ticks=[0.1, 0.3, 0.5, 0.7], orientation='horizontal')
 from PIL import Image
 img = Image.open('/Users/pawan1.dwivedi/Downloads/pics/minimalist/6005397-minimalist-wallpaper.png')
 
-img.thumbnail((64, 64), Image.ANTIALIAS)
+img.thumbnail((256, 256), Image.ANTIALIAS)
 imgplot = plt.imshow(img)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+imgplot = plt.imshow(img, interpolation="nearest")
+imgplot = plt.imshow(img, interpolation="bicubic")
 
 
 # Histogram of image data
 plt.hist(lum_img.ravel(), bins = 256, range = (0.0, 1.0), fc = 'k', ec = 'k')
-
-
 imgplot = plt.imshow(lum_img, clim = (0.0, 0.7))
 
 
 
+# LIFECYCLE OF A PLOT
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
+data = {'Barton LLC': 109438.50,
+        'Frami, Hills and Schmidt': 103569.59,
+        'Fritsch, Russel and Anderson': 112214.71,
+        'Jerde-Hilpert': 112591.43,
+        'Keeling LLC': 100934.30,
+        'Koepp Ltd': 103660.54,
+        'Kulas Inc': 137351.96,
+        'Trantow-Barrows': 123381.38,
+        'White-Trantow': 135841.99,
+        'Will LLC': 104437.60}
+group_data = list(data.values())
+group_names = list(data.keys())
+group_mean = np.mean(group_data)
+
+fig, ax = plt.subplots()
+ax.barh(group_names, group_data)
+
+
+# Controlling the style
+print(plt.style.available)
+plt.style.use('fivethirtyeight')
+
+fig, ax = plt.subplots()
+ax.barh(group_names, group_data)
+
+# rotate x-labels
+fig, ax = plt.subplots()
+ax.barh(group_names, group_data)
+labels = ax.get_xticklabels()
+plt.setp(labels, rotation=45, horizontalalignment='right')
+
+
+# to avoid missing or hidden labels because of too much rotation
+plt.rcParams.update({'figure.autolayout': True})
+fig, ax = plt.subplots()
+ax.barh(group_names, group_data)
+labels = ax.get_xticklabels()
+plt.setp(labels, rotation=45, horizontalalignment='right')
+
+
+# add labels to the plot
+fig, ax = plt.subplots()
+ax.barh(group_names, group_data)
+labels = ax.get_xticklabels()
+plt.setp(labels, rotation=45, horizontalalignment='right')
+ax.set(xlim=[-10000, 140000], xlabel='Total Revenue', ylabel='Company',
+       title='Company Revenue')
+
+
+# adjust the size of the plot
+fig, ax = plt.subplots(figsize = (8, 4)) # figsize = (width, height)
+ax.barh(group_names, group_data)
+labels = ax.get_xticklabels()
+plt.setp(labels, rotation=45, horizontalalignment='right')
+ax.set(xlim=[-10000, 140000], xlabel='Total Revenue', ylabel='Company',
+       title='Company Revenue')
+
+
+# custom formatting guidelines for labels
+def currency(x, pos):
+    if x >= 1e6:
+        s = '${:1.1f}M'.format(x*1e-6)
+    else:
+        s = '${:1.0f}K'.format(x*1e-3)
+    return s
+
+formatter = FuncFormatter(currency)
+
+fig, ax = plt.subplots(figsize=(6, 8))
+ax.barh(group_names, group_data)
+labels = ax.get_xticklabels()
+plt.setp(labels, rotation=45, horizontalalignment='right')
+
+ax.set(xlim=[-10000, 140000], xlabel='Total Revenue', ylabel='Company',
+       title='Company Revenue')
+ax.xaxis.set_major_formatter(formatter) # apply formatter to the labels
+
+
+
+# Combining multiple visualizations
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.barh(group_names, group_data)
+labels = ax.get_xticklabels()
+plt.setp(labels, rotation=45, horizontalalignment='right')
+
+# Add a vertical line, here we set the style in the function call
+ax.axvline(group_mean, ls='--', color='r')
+
+# Annotate new companies
+for group in [3, 5, 8]:
+    ax.text(145000, group, "New Company", fontsize=10,
+            verticalalignment="center")
+
+# Now we'll move our title up since it's getting a little cramped
+ax.title.set(y=1.05)
+
+ax.set(xlim=[-10000, 140000], xlabel='Total Revenue', ylabel='Company',
+       title='Company Revenue')
+ax.xaxis.set_major_formatter(formatter)
+ax.set_xticks([0, 25e3, 50e3, 75e3, 100e3, 125e3])
+fig.subplots_adjust(right=.1)
+
+plt.show()
+
+
+# Saving our plot
+print(fig.canvas.get_supported_filetypes()) # saving options
+# fig.savefig('sales.png', transparent=False, dpi=80, bbox_inches="tight") # to save the plot to disk
+
+
+
+# Customizing matplotlib with style sheets and rcParams
+
+# using style sheets
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from cycler import cycler
+plt.style.use('ggplot')
+data = np.random.randn(50)
+print(plt.style.available)
+
+# defining own style
+# create a file: mpl_configdir/stylelib/presentation.mplstyle
+# axes.titlesize : 24
+# axes.labelsize : 20
+# lines.linewidth : 3
+# lines.markersize : 10
+# xtick.labelsize : 16
+# ytick.labelsize : 16
+# plt.style.use('presentation')
